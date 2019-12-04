58a60c7938bead508c0630a679df285ae86265b4
yckim <rdc3884@gmail.com>
2019-12-05 03:39:57 +0900

    1. twomSum

---
+++ src/TowSum.kt
@@ -0,0 +1,22 @@
+import java.lang.IllegalArgumentException
+
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val map = HashMap<Int, Int>()
+        for ((i, v) in nums.withIndex()) {
+            if (map.containsKey(target - v)) {
+                return intArrayOf(map[target - v]!!, i)
+            }
+            map[v] = i
+        }
+
+        throw IllegalArgumentException()
+    }
+}
+
+fun main() {
+    val s = Solution()
+    val nums = intArrayOf(6,4,3,8,7,5,2)
+    val (result1, result2) = s.twoSum(nums, 5)
+    println("result1=$result1, result2=$result2")
+}
