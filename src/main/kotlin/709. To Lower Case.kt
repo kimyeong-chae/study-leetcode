a5a66cdce86dcb4eae157c879541287261a8316f
yckim <rdc3884@gmail.com>
2019-12-13 01:56:21 +0900

    Add 709. To Lower Case

---
+++ src/main/kotlin/709. To Lower Case.kt
@@ -0,0 +1,11 @@
+class Solution6 {
+    fun toLowerCase(str: String): String {
+        return str.toLowerCase()
+    }
+}
+
+fun main() {
+    val s = Solution6()
+    val result = s.toLowerCase("Hello")
+    println("result=$result")
+}
